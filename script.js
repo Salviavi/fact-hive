929a431feac199b6fafa6b891dc0d0cd947e64b1
Salvia <entp7w8@gmail.com>
2026-02-13 21:17:26 +0700

    adding tag colors based on category, using vanilla javascript

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Thread Lists
 
+const CATEGORIES = [
+  { name: "technology", color: "#b9fbc0" },
+  { name: "science", color: "#cfbaf0" },
+  { name: "psychology", color: "#ffcfd2" },
+  { name: "society", color: "#96BDC6" },
+  { name: "history", color: "#FFDAB3" },
+];
+
+console.log(CATEGORIES.find((cat) => cat.name === "society").color);
+
 const initialFacts = [
   {
     id: 1,
@@ -41,14 +51,67 @@ const threadsList = document.querySelector(".threads-list");
 
 console.dir(btn);
 
+// Load data from Supabase
+
+loadThread();
+
+async function loadThread() {
+  const res = await fetch(
+    "https://gaztihhknenpfqcyowjg.supabase.co/rest/v1/facthive",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhenRpaGhrbmVucGZxY3lvd2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDI0MzYsImV4cCI6MjA4NTMxODQzNn0.IeElT9OhDXXPGHI3-cIXD49gSXLQWoN0QIvXAL45SAs",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhenRpaGhrbmVucGZxY3lvd2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDI0MzYsImV4cCI6MjA4NTMxODQzNn0.IeElT9OhDXXPGHI3-cIXD49gSXLQWoN0QIvXAL45SAs",
+      },
+    },
+  );
+
+  const data = await res.json();
+  console.log(res);
+  console.log(data);
+
+  /*
+
+  filteringData = (data) => {
+    const result = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].category === "psychology") {
+        result.push(data[i]);
+      }
+    }
+
+    return result;
+  };
+
+  */
+
+  createThreadsList(data);
+}
+
 // Create DOM elements: Render facts in list
 threadsList.innerHTML = "";
 
 threadsList.insertAdjacentHTML("afterbegin", "<li>Yono</li>");
 function createThreadsList(dataArray) {
-  const htmlArray = dataArray.map(
-    (fact) => `<li class="thread"><p>
-                ${fact.text}
+  const htmlArray = dataArray.map((fact) => {
+    // Find the matching category color
+
+    let matchedCategory;
+
+    for (let i = 0; i < CATEGORIES.length; i++) {
+      if (CATEGORIES[i].name === fact.category) {
+        matchedCategory = CATEGORIES[i];
+        break;
+      }
+    }
+
+    //
+
+    return `<li class="thread"><p>
+                ${fact.thread}
                 <a
                   class="link-source"
                   href=${fact.source}
@@ -56,10 +119,10 @@ function createThreadsList(dataArray) {
                   >(Source)</a
                 >
               </p>
-              <span class="tag" style="background-color: #cfbaf0"
+              <span class="tag" style="background-color: ${matchedCategory.color}"
                 >${fact.category}</span
-              ></li>`,
-  );
+              ></li>`;
+  });
 
   console.log(htmlArray);
 
@@ -67,7 +130,7 @@ function createThreadsList(dataArray) {
   threadsList.insertAdjacentHTML("afterbegin", html);
 }
 
-createThreadsList(initialFacts);
+// createThreadsList(initialFacts);
 
 // Toggle form visibily
 
@@ -80,3 +143,34 @@ btn.addEventListener("click", function () {
     btn.textContent = "Post a Fact";
   }
 });
+
+console.log(
+  "hehe",
+  [7, 64, 6, -23, 11].filter((el) => el < 10),
+);
+
+const numbers = [7, 64, 6, -23, 11];
+const result = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 10) {
+    result.push(numbers[i]);
+  }
+}
+
+console.log("hehe", result);
+
+console.log(
+  "haha",
+  [7, 64, 6, -23, 11].find((el) => el < 10),
+);
+
+function myFind(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 10) {
+      return array[i];
+    }
+  }
+}
+
+console.log("haha2", myFind(numbers));
