959def80048f98f61c0840fc85823527322e1fa0
Salvia <entp7w8@gmail.com>
2026-02-20 21:10:13 +0700

    separating UI to components, toggle form on UI, adding useState to manage form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
+import { useState } from "react";
 import "./style.css";
 import logo from "./assets/FactHiveLogo.png";
-
-const CATEGORIES = [
-  { name: "technology", color: "#b9fbc0" },
-  { name: "science", color: "#cfbaf0" },
-  { name: "psychology", color: "#ffcfd2" },
-  { name: "society", color: "#96BDC6" },
-  { name: "history", color: "#FFDAB3" },
-];
 
 const initialFacts = [
   {
@@ -43,26 +36,32 @@ const initialFacts = [
   },
 ];
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(5);
+
   return (
     <>
-      {/* HEADERS */}
-      <header className="header">
-        <div className="header-left">
-          <div className="logo">
-            <img src={logo} height="68px" alt="Fact Hive Logo" />
-            <h1>Fact Hive</h1>
-          </div>
-          <div className="tagline">
-            <p>Share facts that matter!</p>
-          </div>
-        </div>
+      <span style={{ fontSize: "50px", marginRight: "20px" }}>{count}</span>
+      <button
+        className="btn btn-large"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        +1
+      </button>
+    </>
+  );
+}
 
-        <button className="btn btn-large btn-open">Post a Fact</button>
-      </header>
+function App() {
+  /* 1. defining state variable */
+  const [showForm, setShowForm] = useState(false);
 
-      <ThreadForm />
+  return (
+    <>
+      <Header showForm={showForm} setShowForm={setShowForm} />
 
+      {/* 2. using state variable */}
+      {showForm ? <ThreadForm /> : null}
       <main className="app-main">
         <CategoryFilter />
         <ThreadList />
@@ -71,12 +70,97 @@ function App() {
   );
 }
 
+function Header({ showForm, setShowForm }) {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <div className="logo">
+          <img src={logo} height="68px" alt="Fact Hive Logo" />
+          <h1>Fact Hive</h1>
+        </div>
+        <div className="tagline">
+          <p>Share facts that matter!</p>
+        </div>
+      </div>
+
+      <button
+        className="btn btn-large btn-open"
+        /* 3. updating state variable */
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? "Close" : "Post a Fact"}
+      </button>
+    </header>
+  );
+}
+
+const CATEGORIES = [
+  { name: "technology", color: "#b9fbc0" },
+  { name: "science", color: "#cfbaf0" },
+  { name: "psychology", color: "#ffcfd2" },
+  { name: "society", color: "#96BDC6" },
+  { name: "history", color: "#FFDAB3" },
+];
+
 function ThreadForm() {
-  return <form className="thread-form">Thread Form</form>;
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="thread-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a mind-blowing fact..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        placeholder="Drop the fact source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function ThreadList() {
@@ -92,31 +176,42 @@ function ThreadList() {
           );
 
           return (
-            <li key={fact.id} className="thread">
-              <p>
-                {fact.text}
-                <a className="link-source" href={fact.source} target="_blank">
-                  (Source)
-                </a>
-              </p>
-              <span
-                className="tag"
-                style={{
-                  backgroundColor: matchedCategory?.color || "#ccc",
-                }}
-              >
-                {fact.category}
-              </span>
-              <div className="vote-buttons">
-                <button>👍 {fact.votesInteresting}</button>
-                <button>🤯 {fact.votesMindblowing}</button>
-                <button>⛔ {fact.votesFalse}</button>
-              </div>
-            </li>
+            <Thread
+              key={fact.id}
+              fact={fact}
+              matchedCategory={matchedCategory}
+            />
           );
         })}
       </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
     </section>
+  );
+}
+
+function Thread({ fact, matchedCategory }) {
+  return (
+    <li key={fact.id} className="thread">
+      <p>
+        {fact.text}
+        <a className="link-source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: matchedCategory?.color || "#ccc",
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔ {fact.votesFalse}</button>
+      </div>
+    </li>
   );
 }
 
